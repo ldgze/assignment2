7639293cf12606303289d333cf2bce23e29d28d9
dongze li <ezgnod@gmail.com>
2023-10-05 00:10:12 -0700

    Finished the assignment

--- js/main.js
+++ js/main.js
@@ -1,31 +1,37 @@
 function MainModule(listingsID = "#listings") {
   const me = {};
 
-
   const listingsElement = document.querySelector(listingsID);
 
   function getListingCode(listing) {
-    return `<div class="col-4">
-  <div class="listing card">
-    <img
-      src="https://a0.muscache.com/pictures/b7c2a199-4c17-4ba6-b81d-751719d2dac6.jpg"
-      class="card-img-top"
-      alt="AirBNB Listing"
-    />
-    <div class="card-body">
-      <h2 class="card-title">${listing.name}</h2>
-      <div>${listing.price}</div>
-      <p class="card-text">
-        Some quick example text to build on the card title and make up
-        the bulk of the card's content.
-      </p>
-      <a href="#" class="btn btn-primary">Go somewhere</a>
-    </div>
-  </div>
-  <!-- /card -->
-  </div>
+    const amenities = Array.isArray(listing.amenities)
+      ? listing.amenities
+      : JSON.parse(listing.amenities);
 
-  `;
+    const amenitiesHTML = amenities
+      .map((amenity) => `<label>${amenity}</label>`)
+      .join("");
+
+    return `<div class="col-4">
+      <div class="listing card">
+        <img
+          src=${listing.picture_url}
+          class="card-img-top"
+          alt="AirBNB Listing"
+        />
+        <div class="card-body">
+          <h2 class="card-title">${listing.name}</h2>
+          <div class="price">${listing.price}</div>
+          <p class="card-text">${listing.description}</p>
+          <p class="card-text amenities-title">Amenities</p>
+          <div class="card-text amenities">${amenitiesHTML}</div>
+          <div class="card-text host-info">
+            <img class="host-picture" src=${listing.host_picture_url} alt=${listing.host_name} />
+            <span class="host-name">${listing.host_name}</span>
+          </div>
+        </div>
+      </div>
+    </div>`;
   }
 
   function redraw(listings) {
@@ -46,7 +52,6 @@ function MainModule(listingsID = "#listings") {
     const res = await fetch("./airbnb_sf_listings_500.json");
     const listings = await res.json();
 
-
     me.redraw(listings.slice(0, 50));
   }
 
@@ -57,6 +62,5 @@ function MainModule(listingsID = "#listings") {
 }
 
 const main = MainModule();
-
 
 main.loadData();
